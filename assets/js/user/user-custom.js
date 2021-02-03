55855fb3a77ac1c0b49c7cb9063060015b38e714
aakhil.citrusbug <vyasishanatc194@gmail.com>
2021-02-03 19:34:03 +0530

    updated

--- assets/js/user/user-custom.js
+++ assets/js/user/user-custom.js
@@ -29,4 +29,38 @@ $(document).ready(function () {
       sticky.removeClass("header-bgcolor slideInDown animated");
     }
   });
+
+
+  $(".owl-carousel-detail").owlCarousel({
+    loop: true,
+    margin: 12,
+    smartSpeed: 2000,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    responsiveClass: true,
+    navText: [
+      "<i class='fe fe-arrow-left'></i>",
+      "<i class='fe fe-arrow-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+        nav: true,
+      },
+      1000: {
+        items: 1,
+        nav: true,
+      },
+      1025: {
+        items: 1,
+        nav: true,
+      },
+    },
+  });
 });
